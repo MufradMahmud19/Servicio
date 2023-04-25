3ec443223db844b4d2d4fee88f06319d0f9efb4c
Mufrad <66878837+MufradMahmud19@users.noreply.github.com>
2023-04-25 10:35:09 +0600

    Add files via upload

---
+++ admin/add_content.php
@@ -0,0 +1,100 @@
+<?php
+
+include '../components/connect.php';
+
+if(isset($_COOKIE['tutor_id'])){
+   $tutor_id = $_COOKIE['tutor_id'];
+}else{
+   $tutor_id = '';
+   header('location:login.php');
+}
+
+if(isset($_POST['submit'])) {
+
+   $id = unique_id();
+   $status = $_POST['status'];
+   $status = filter_var($status, FILTER_SANITIZE_STRING);
+   $title = $_POST['title'];
+   $title = filter_var($title, FILTER_SANITIZE_STRING);
+   $description = $_POST['description'];
+   $description = filter_var($description, FILTER_SANITIZE_STRING);
+   $playlist = $_POST['playlist'];
+   $playlist = filter_var($playlist, FILTER_SANITIZE_STRING);
+
+      $add_playlist = $conn->prepare("INSERT INTO `content`(id, tutor_id, playlist_id, title, description, status) VALUES(?,?,?,?,?,?)");
+      $add_playlist->execute([$id, $tutor_id, $playlist, $title, $description, $status]);
+      $message[] = 'new service uploaded!';
+
+}
+else{
+   echo "file couldn't uploded";
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Dashboard</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="../css/admin_style.css">
+
+</head>
+<body>
+
+<?php include '../components/admin_header.php'; ?>
+   
+<section class="video-form">
+
+   <h1 class="heading">upload content</h1>
+
+   <form action="" method="post" enctype="multipart/form-data">
+      <p>video status <span>*</span></p>
+      <select name="status" class="box" required>
+         <option value="" selected disabled>-- select status</option>
+         <option value="active">active</option>
+         <option value="deactive">deactive</option>
+      </select>
+      <p>video title <span>*</span></p>
+      <input type="text" name="title" maxlength="100" required placeholder="enter video title" class="box">
+      <p>video description <span>*</span></p>
+      <textarea name="description" class="box" required placeholder="write description" maxlength="1000" cols="30" rows="10"></textarea>
+      <p>video playlist <span>*</span></p>
+      <select name="playlist" class="box" required>
+         <option value="" disabled selected>--select service</option>
+         <?php
+         $select_playlists = $conn->prepare("SELECT * FROM `playlist` WHERE tutor_id = ?");
+         $select_playlists->execute([$tutor_id]);
+         if($select_playlists->rowCount() > 0){
+            while($fetch_playlist = $select_playlists->fetch(PDO::FETCH_ASSOC)){
+         ?>
+         <option value="<?= $fetch_playlist['id']; ?>"><?= $fetch_playlist['title']; ?></option>
+         <?php
+            }
+         ?>
+         <?php
+         }else{
+            echo '<option value="" disabled>no playlist created yet!</option>';
+         }
+         ?>
+      </select>
+      <input type="submit" value="upload video" name="submit" class="btn">
+   </form>
+
+</section>
+
+
+
+<?php include '../components/footer.php'; ?>
+
+<script src="../js/admin_script.js"></script>
+
+</body>
+</html>
---
+++ components/user_header.php
@@ -0,0 +1,73 @@
+<?php
+if(isset($message)){
+   foreach($message as $message){
+      echo '
+      <div class="message">
+         <span>'.$message.'</span>
+         <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
+      </div>
+      ';
+   }
+}
+?>
+
+<header class="header">
+
+      <section class="flex">
+
+      <a href="home.php" class="logo">Servicio</a>
+      
+      
+            <nav class="navbar">
+               <a href="home.php">Home</a>
+        
+             <a href="courses.php"></i>Services</a>
+       
+            <a href="teachers.php"><span>Employees</span></a>
+      
+              <a href="contact.php"><span>Contact</span></a>
+            </nav>
+        
+
+      <div class="icons">
+         <div id="search-btn" class="fas fa-search"></div>
+         <div id="user-btn" class="fas fa-user"></div>
+      </div>
+
+      <div class="profile">
+         <?php
+            $select_profile = $conn->prepare("SELECT * FROM `users` WHERE id = ?");
+            $select_profile->execute([$user_id]);
+            if($select_profile->rowCount() > 0){
+            $fetch_profile = $select_profile->fetch(PDO::FETCH_ASSOC);
+         ?>
+         <img src="uploaded_files/<?= $fetch_profile['image']; ?>" alt="">
+         <h3><?= $fetch_profile['name']; ?></h3>
+         <span>user</span>
+         <a href="profile.php" class="btn">view profile</a>
+         <div class="flex-btn">
+            <a href="login.php" class="option-btn">login</a>
+            <a href="register.php" class="option-btn">register</a>
+         </div>
+         <a href="components/user_logout.php" onclick="return confirm('logout from this website?');" class="delete-btn">logout</a>
+         <?php
+            }else{
+         ?>
+         <h3>please login or register</h3>
+          <div class="flex-btn">
+            <a href="login.php" class="option-btn">login</a>
+            <a href="register.php" class="option-btn">register</a>
+         </div>
+         <?php
+            }
+         ?>
+      </div>
+</section>
+
+</header>
+
+
+
+<!-- header section ends -->
+
+
